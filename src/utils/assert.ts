8cd1a3a5423f462c289ca90e3023b5de3dc9ad4a
RicoPup <rico-dawg@hotmail.co.uk>
2025-09-10 14:06:14 -0400

    Update assert() type assertion

--- src/utils/assert.ts
+++ src/utils/assert.ts
@@ -1,12 +1,20 @@
 import { ApiError } from "./error.js"
 
-export const assert = (data: any, message = 'Required data is missing', status = 400, callback?: () => void) => {
+export const assert: <T>(
+    data: T,
+    message?: string,
+    status?: number,
+    callback?: () => void
+) => asserts data is NonNullable<T> = (
+    data,
+    message = 'Required data is missing',
+    status = 400,
+    callback
+) => {
     if (data === undefined || data === null) {
         if (callback) {
             callback()
         }
-        else {
-            throw new ApiError(status, message)
-        }
+        throw new ApiError(status, message)
     }
 }
